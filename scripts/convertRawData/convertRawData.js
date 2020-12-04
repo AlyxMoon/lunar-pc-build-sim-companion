8b0ef69321b072c93cff3ec0b2d998a2d7b8c2de
Allister <allisterkmoon@gmail.com>
2020-12-03 16:45:28 -0800

    get parts data and convert it for use in the app

---
+++ scripts/convertRawData/convertRawData.js
@@ -0,0 +1,48 @@
+const fs = require('fs')
+const path = require('path')
+const { promisify } = require('util')
+
+const partCategories = [
+  'casefans',
+  'cases',
+  'cpu-coolers',
+  'cpus',
+  'gpus',
+  'memory',
+  'motherboards',
+  'power-supplies',
+  'storage',
+]
+
+const convertRawData = raw => {
+  const [headers, ...rows] = raw.split('\n').map(line => line.trim().split(','))
+
+  return rows.map(row => {
+    const formatted = {}
+
+    for (const col in row) {
+      formatted[headers[col]] = row[col]
+    }
+
+    return formatted
+  })
+}
+
+const main = async () => {
+  const finalDirectory = path.join(__dirname, '../../src/assets/data/parts')
+  if (!fs.existsSync(finalDirectory)) {
+    await promisify(fs.mkdir)(finalDirectory)
+  }
+
+  for (const category of partCategories) {
+    const rawFilePath = path.join(__dirname, 'data', category + '.csv')
+    const formattedFilePath = path.join(finalDirectory, category + '.json')
+
+    const rawData = await promisify(fs.readFile)(rawFilePath, 'utf-8')
+    const formattedData = JSON.stringify(convertRawData(rawData), null, 2)
+
+    await promisify(fs.writeFile)(formattedFilePath, formattedData, 'utf-8')
+  }
+}
+
+main()
