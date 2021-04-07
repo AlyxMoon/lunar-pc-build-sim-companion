2c4c34a942c2c54172879fb224150446e522d41c
AlyxMoon <allisterkmoon@gmail.com>
2021-04-07 08:34:18 -0700

    update production URL

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/lunar-pc-build-sim-companion/'
+    ? '/pc-build-sim-companion/'
     : '/',
   css: {
     loaderOptions: {
